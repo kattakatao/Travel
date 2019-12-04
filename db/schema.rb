d7a85c587d3a96f0d797472604e8b26260351142
kattakatao <kattakatao1018@gmail.com>
2019-12-04 13:59:45 +0000

    微調整

--- db/schema.rb
+++ db/schema.rb
@@ -10,7 +10,7 @@
 #
 # It's strongly recommended that you check this file into your version control system.
 
-ActiveRecord::Schema.define(version: 2019_12_04_122536) do
+ActiveRecord::Schema.define(version: 2019_12_04_031605) do
 
   create_table "admin_users", force: :cascade do |t|
     t.string "email", default: "kattakatao@icliud.com", null: false
